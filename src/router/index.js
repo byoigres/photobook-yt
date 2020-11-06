14440c1ccecca3950af51c6057f3820ff08b7ed2
Sergio Flores <byoigres@gmail.com>
2020-11-06 10:34:34 -0700

    Added sing up, albums and album details views with routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,49 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import SignUpPage from "../views/SingUp"
+import AlbumDetailPage from "../views/AlbumDetail"
+import AlbumsPage from "../views/Albums"
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/signup",
+    name: "SingUpPage",
+    component: SignUpPage,
+  },
+  {
+    path: "/album/:id",
+    name: "AlbumDetailPage",
+    component: AlbumDetailPage,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/albums",
+    name: "AlbumsPage",
+    component: AlbumsPage,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
